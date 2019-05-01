29789280f21d77cc149399ac5385c25c31303b2f
ninnat <truecrimson@gmail.com>
2019-05-01 16:01:23 +0000

    Updating to 92b137c6429e59ff51cdc93947a5676d48de68bd.

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -3,7 +3,7 @@
   $.fn.toc = function(options) {
     var defaults = {
       noBackToTopLinks: false,
-      title: '<i>Contents</i>',
+      title: '<b>Contents</b>',
       minimumHeaders: 3,
       headers: 'h1, h2, h3, h4, h5, h6',
       listType: 'ol', // values: [ol|ul]
